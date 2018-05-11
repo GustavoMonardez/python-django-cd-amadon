67a2337aeee854a547fcdaa43da1f55b33ab1b90
RAINFOREST\Gustavo <gustavomonardez@gmail.com>
2018-05-10 19:00:08 -0700

    added sessions

--- apps/amadon_app/views.py
+++ apps/amadon_app/views.py
@@ -8,14 +8,25 @@ def index(request):
     return render(request, "amadon_app/index.html", context)
 
 def buy(request):
-    print(request.POST["quantity"])
-    print(request.POST["product_id"])
+    # total items
+    if not "count" in request.session:
+        request.session["count"] = 0
+    # total spent
+    if not "total" in request.session:
+        request.session["total"] = 0
+    # current item
     product_id = request.POST["product_id"]
+    product_quantity = int(request.POST["quantity"])
+    # products info
     products = products_list()
+    # find price for item in list
     for prod in products:
-        if str(prod["id"]) == product_id:
-            print("product price: ",prod["price"])
-        print(prod["id"])
+        if prod["id"] == product_id:
+            request.session["subtotal"] = product_quantity * prod["price"]
+            request.session["total"] += round(request.session["subtotal"],2)
+    # update total items
+    request.session["count"] += product_quantity
+
     return redirect("/amadon_app/checkout")
 
 def checkout(request):
@@ -23,9 +34,9 @@ def checkout(request):
     return render(request, "amadon_app/checkout.html")
 
 def products_list():
-    products = [{"id":1,"name":"Dojo T-Shirt","price":"$19.99" }, 
-                {"id":2,"name":"Dojo Sweater","price":"$29.99" },
-                {"id":3,"name":"Dojo Cup","price":"$4.99" },
-                {"id":4,"name":"Algorithm Book","price":"$49.99" }              
+    products = [{"id":"1","name":"Dojo T-Shirt","price":19.99 }, 
+                {"id":"2","name":"Dojo Sweater","price":29.99 },
+                {"id":"3","name":"Dojo Cup","price":4.99 },
+                {"id":"4","name":"Algorithm Book","price":49.99 }              
             ]
     return products
